4ba410e82ece0ee64970aa01485e1823091f823f
LordSyd <daniel.kleissl@gmx.at>
2019-12-13 08:30:05 +0100

    added new logic for tracking click_pos: HashMap that takes 2 pos, saves at same key

--- link_map.py
+++ link_map.py
@@ -11,9 +11,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 screen = pygame.display.set_mode((500, 500))
+running = True
 
-connections_list = []
-num_clicks = 1
+connections_key_list = []
+num_clicks = 0
+
+LEFT = 1
 
 
 def get_instance_name(position):
@@ -45,53 +48,89 @@ used to draw the marker at a given position
   def check_click(self, mouse):
     if self.rect.collidepoint(mouse):
       self.image.set_alpha(200)
-      return True
     if not self.rect.collidepoint(mouse):
       self.image.set_alpha(50)
 
-  def get_sprite_position(self):
-    return (self.rect.x, self.rect.y)
 
+class ConnectionHashMap:
+  def __init__(self, array_size):
+    self.array_size = array_size
+    self.array = [None for item in range(array_size)]
 
-class Connection:
-  """
-  used as a container for an entrance/exit-pair, self.set_entrance_or_exit(position) 
-  is used to assign clicked coordinates
-  """
+  def hash(self, key, count_collisions=0):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code + count_collisions
 
-  def __init__(self, full=False):
-    self.entrance = None
-    self.exit = None
-    self.full = full
+  def compressor(self, hash_code):
+    return hash_code % self.array_size
 
-  def set_entrance_or_exit(self, position):
-    if self.check_if_full():
-      print('full')
+  def assign(self, key, value):
+    array_index = self.compressor(self.hash(key))
+    current_array_value = self.array[array_index]
+
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
       return
-    elif self.entrance is None:
-      self.entrance = position
-    elif not self.entrance is None and self.exit is None:
-      self.exit = position
-      self.full = True
 
-  def get_entrance(self):
-    if self.entrance is None:
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
       return
-    else:
-      return self.entrance
 
-  def get_exit(self):
-    if self.exit is None:
-      return
-    else:
-      return self.exit
+    # Collision!
 
-  def check_if_full(self):
-    return self.full
+    number_collisions = 1
+
+    while(current_array_value[0] != key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
+        return
+
+      number_collisions += 1
+
+    return
+
+  def retrieve(self, key):
+    array_index = self.compressor(self.hash(key))
+    possible_return_value = self.array[array_index]
+
+    if possible_return_value is None:
+      return None
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
+        return possible_return_value[1]
+
+      retrieval_collisions += 1
+
+    return
 
 
+connections = ConnectionHashMap(200)
+
+#test2 = Marker(10, 10)
 all_sprites = pygame.sprite.Group()
-
+# all_sprites.add(test2)
 
 running = True
 
@@ -100,30 +139,32 @@ while running:
     for s in all_sprites:
       mouse_pos = pygame.mouse.get_pos()
       s.check_click(mouse_pos)
-      # print(type(s))
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-    elif event.type == pygame.MOUSEBUTTONDOWN:
+    elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+      mouse = pygame.mouse.get_pos()
       num_clicks += 1
-      var = get_instance_name(pygame.mouse.get_pos())
-      var = Connection()
-      # connections_list.append(var)
-      var.set_entrance_or_exit(pygame.mouse.get_pos())
+      if num_clicks % 3 == 1:
+        var = get_instance_name(mouse)
+        connections_key_list.append(var)
+        connections.assign(connections_key_list[-1], mouse)
+      elif num_clicks % 3 == 2:
+        old_pos = connections.retrieve(connections_key_list[-1])
+        connections.assign(connections_key_list[-1], (old_pos, mouse))
 
-      print(var.get_exit())
-      print(var.get_entrance())
       # print(all_sprites)
-      # print(connections_list[0].get_entrance())
-      s, j = var.get_entrance()  # gets mouse position
-      test = Marker(s, j)
-      all_sprites.add(test)
 
   screen.fill(BLACK)
   all_sprites.update()
   all_sprites.draw(screen)
   pygame.display.update()
+  print((num_clicks % 3))
+  for i in connections_key_list:
+    var = connections.retrieve(i)
+
+    print('key: ' + i + '; value: ' + str(var))
 
 
 pygame.quit()
